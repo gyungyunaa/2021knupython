270e64bee18261a01f5ca980f020d04c62974595
taegyuna0826@naver.com <taegyuna0826@naver.com>
2021-07-23 08:59:07 +0900

    second week

---
+++ 3.recursion+condition/double_for_recursion.py
@@ -0,0 +1,7 @@
+# for문 속에 for문 / 이중 for문
+for i in range(10):
+    for j in range(1,11):
+        print(j, end="")
+    print()
+
+
---
+++ 3.recursion+condition/procedure.py
@@ -0,0 +1,11 @@
+# 순차 구조 : 줄넘버 1번부터 N번까지 순서대로 진행 
+# 2.variables+datatypes 예제들 참고하기
+# 프로그래밍의 흐름에서 가장 기본적인 룰!
+
+# 정수형
+a = 154
+print(type(a))
+a = 0
+print(type(a))
+a = -25
+print(type(a))
